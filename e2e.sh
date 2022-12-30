1c88dae411ed07bd3e6ce098ba8897cd07bf4f9a
gregclinker <gregory.clinker@gmail.com>
2022-12-30 18:39:28 +0000

    new script

---
+++ e2e.sh
@@ -0,0 +1,11 @@
+tag=v1.2
+#
+./pr-clean-up.sh
+#
+./pr-test.sh patch/test1 ; ./pr-test.sh feature/test2 ; ./pr-test.sh release/test3
+#
+./pr-merge.sh patch/test1
+./pr-merge.sh feature/test2
+./pr-merge.sh release/test3
+#
+git checkout main ; git pull ; git tag $tag ; git push origin $tag
