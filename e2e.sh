41478dd194ce6e55914091dc4f77e3eb4e65288e
gregclinker <gregory.clinker@gmail.com>
2022-12-30 18:54:57 +0000

    pr clean up

--- e2e.sh
+++ e2e.sh
@@ -1,12 +1,12 @@
-tag=v1.2
+tag=v1.3
 #
 ./pr-clean-up.sh
 #
-./pr-test.sh patch/test1 ; ./pr-test.sh feature/test2 ; ./pr-test.sh release/test3
+./pr-test.sh patch/test11 ; ./pr-test.sh feature/test21 ; ./pr-test.sh release/test31
 #
-./pr-merge.sh patch/test1
-./pr-merge.sh feature/test2
-./pr-merge.sh release/test3
+./pr-merge.sh patch/test11
+./pr-merge.sh feature/test21
+./pr-merge.sh release/test31
 #
 git checkout main ; git pull ; git tag $tag ; git push origin $tag
 #
