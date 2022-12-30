9d9bf0e66f915a75581d36c57653b5c609c92324
gregclinker <gregory.clinker@gmail.com>
2022-12-30 09:52:37 +0000

    change for branch feature/test1

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ change for branch feature/test-1
 change for branch feature/test1
 change for branch feature/test1
 change for branch feature/test1
+change for branch feature/test1
--- pr-test.sh
+++ pr-test.sh
@@ -3,6 +3,7 @@ git checkout main
 git pull
 git push -d origin $1
 git checkout -b $1
+echo "XXXXXXXXXXXXXXXXXXX"
 git status
 echo "change for branch $1" >> README.md 
 git add --all 
