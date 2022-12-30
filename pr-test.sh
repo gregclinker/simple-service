88702ab9d8085af9a19488bbe1a93b90c0df9b55
gregclinker <gregory.clinker@gmail.com>
2022-12-30 11:45:55 +0000

    changed pr script

--- pr-test.sh
+++ pr-test.sh
@@ -6,9 +6,8 @@ git branch -D $1
 git push origin --delete $1
 git pull
 git checkout -b $1
-echo "XXXXXXXXXXXXXXXXXXX"
 git status
-echo "change for branch $1" >> README.md 
+echo "change for branch $1" > README.md 
 git add --all 
 git commit -m "change for branch $1" 
 git push origin $1
