be252c1bbf151f6e8c2b82de7a17dd794631056a
gregclinker <gregory.clinker@gmail.com>
2022-12-30 10:22:24 +0000

    Update pr-test.sh

--- pr-test.sh
+++ pr-test.sh
@@ -7,7 +7,7 @@ git pull
 git checkout -b $1
 echo "XXXXXXXXXXXXXXXXXXX"
 git status
-echo "change for branch $1" >> README.md 
+echo "change for branch $1\" >> README.md 
 git add --all 
 git commit -m "change for branch $1" 
 git push origin $1
